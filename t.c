2cc716238f1a4a97d14f495657fb414a98ce607c
Fabrice Pardo <fabrice.pardo@universite-paris-saclay.fr>
2021-07-14 15:24:59 +0200

    Define main with no arguments

--- t.c
+++ t.c
@@ -3,31 +3,31 @@
 char *S1 = "@n'+,#'/*{}w+/w#cdnr/+,{}r/*de}+,/*{*+,/w{%+,/w#q#n+,/#{l+,/n{n+,/+#n+,/#;#q#n+,/+k#;*+,/'r :'d*'3,}{w+K w'K:'+}e#';dq#'l q#'+d'K#!/+k#;q#'r}eKK#}w'r}eKK{nl]'/#;#q#n'){)#}w'){){nl]'/+#n';d}rw' i;# ){nl]!/n{n#'; r{#w'r nc{nl]'/#{l,+'K {rw' iK{;[{nl]'/w#q#n'wk nw' iwk{KK{nl]!/w{%'l' i; :{nl]'/*{q#'ld;r'}{nlwb!/*de}'c ;;{nl'-{}rw]'/+,}##'*}#nc,',#nw]'/+kd'+e}+;#'rdq#w! nr'/ ') }+}{rl#'{n' ')# }'+}##(!!/";
 char *S2 = "!ek;dc i@bK'(q)-[w]*%n+r3#l,{}:\nuwloca-O;m .vpbks,fxntdCeghiry";
 
-int main(int t,int c,char *a) 
+int f(int t,int c,char *a) 
 { 
 return !0 < t
   ?
   t < 3
   ?
-  main(-79,
+  f(-79,
        -13,
-       a + main(-87,
+       a + f(-87,
 		1 - c,
-		a + main(-86, 0, a+1 )
+		a + f(-86, 0, a+1 )
 		)
        )
   :
   1,
   t < c
   ?
-  main(t + 1, c, a)
+  f(t + 1, c, a)
   :
   3,
-  main(-94, -27 + t, a) && t == 2
+  f(-94, -27 + t, a) && t == 2
   ?
   c < 13
   ? 
-  main(2, c+1, "%s %d %d\n")
+  f(2, c+1, "%s %d %d\n")
   :9
   :
   16
@@ -35,7 +35,7 @@ return !0 < t
   t < 0
   ?
   t < -72
-  ?main(c,
+  ?f(c,
 	t, 
 	S1
 	) 
@@ -45,19 +45,23 @@ return !0 < t
   ?
   putchar(32[a-1])
   :
-  main(-65, c, a+1)
+  f(-65, c, a+1)
   :
-  main((*a == '/') + t, c, a+1) 
+  f((*a == '/') + t, c, a+1) 
   :
   0 < t
   ?
-  main(2, 2, "%s")
+  f(2, 2, "%s")
   :
-  *a == '/' || main(0,
-		    main(-61,
+  *a == '/' || f(0,
+		    f(-61,
 			 *a,
 			 S2
 			 ),
 		    a+1
 		    ); 
+}
+
+int main() {
+  return f(1, 0, 0);
 }
