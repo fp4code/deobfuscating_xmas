099486891e54d9eab3f55be5bcc86f401fa4e1cb
Fabrice Pardo <fabrice.pardo@universite-paris-saclay.fr>
2021-07-14 18:16:38 +0200

    Introduce f0

--- t.c
+++ t.c
@@ -3,6 +3,19 @@
 char *S1 = "@n'+,#'/*{}w+/w#cdnr/+,{}r/*de}+,/*{*+,/w{%+,/w#q#n+,/#{l+,/n{n+,/+#n+,/#;#q#n+,/+k#;*+,/'r :'d*'3,}{w+K w'K:'+}e#';dq#'l q#'+d'K#!/+k#;q#'r}eKK#}w'r}eKK{nl]'/#;#q#n'){)#}w'){){nl]'/+#n';d}rw' i;# ){nl]!/n{n#'; r{#w'r nc{nl]'/#{l,+'K {rw' iK{;[{nl]'/w#q#n'wk nw' iwk{KK{nl]!/w{%'l' i; :{nl]'/*{q#'ld;r'}{nlwb!/*de}'c ;;{nl'-{}rw]'/+,}##'*}#nc,',#nw]'/+kd'+e}+;#'rdq#w! nr'/ ') }+}{rl#'{n' ')# }'+}##(!!/";
 char *S2 = "!ek;dc i@bK'(q)-[w]*%n+r3#l,{}:\nuwloca-O;m .vpbks,fxntdCeghiry";
 
+
+int f0(int c, char *a)
+{
+     if (*a == '/') {
+	  return 1;
+     }
+     char *s = S2;
+     while (*a != *s++);
+     putchar(s[30]);
+     return f0(0, a + 1); // unused first argument
+}
+     
+
 int f(int t, int c, char *a)
 {
      if (t < 0)
@@ -12,18 +25,10 @@ int f(int t, int c, char *a)
 	      return f(t, c, a + 1);
 	  }
      if (t == 0)
-	  if (*a == '/') {
-	      return 1;
-	  }
-	  else {
-	       char *s = S2;
-	       while (*a != *s++);
-	       putchar(s[30]);
-	       return f(0, 0, a + 1); // unused second argument
-	  }
+	  return f0(c, a);
      if (t < 3) {
 	  // passe 12 fois ici
-	  f(0, -86, S1);
+	  f0(-86, S1);
 	  f(1 - c, -87, S1);
 	  f(-13, -79, S1);
      }
